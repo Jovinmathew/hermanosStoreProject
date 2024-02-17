2ba8235a6b2f164cffc7cbcf1a7d5147c13da882
Jovinmathew <42744096+Jovinmathew@users.noreply.github.com>
2024-02-17 17:46:12 +0530

    Added Filter
    Filter categories on home

--- lib/screens/home/screen.dart
+++ lib/screens/home/screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -6,34 +6,111 @@ import 'package:hermanos/components/thumbnail/component.dart';
 import 'package:hermanos/models/product/model.dart';
 import 'package:hermanos/services/product/service.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
-  ProductService get productService => GetIt.I<ProductService>();
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late List<Product> _products;
+  late ProductService _productService;
+
+  @override
+  void initState() {
+    super.initState();
+    _productService = GetIt.I<ProductService>();
+    _products = [];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: FutureBuilder(
-          future: productService.getAll(),
-          builder: (_, AsyncSnapshot<List<Product>> snapshot) {
-            if (!snapshot.hasData) {
+      body: Center(
+        child: FutureBuilder(
+          future: _products.isEmpty
+              ? _productService.getAll()
+              : Future.value(_products),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
               return CircularProgressIndicator();
             }
-            final products = snapshot.data!;
             return ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (_, index) {
-                  final product = products[index];
-                  return ListTile(
-                    leading: Thumbnail(imageUrl: product.image),
-                    title: Text(product.title),
-                    subtitle: Text(product.category),
-                    trailing: Text(product.price.toString()),
-                  );
-                });
-          }),
-    ));
+              itemCount: _products.length,
+              itemBuilder: (_, index) {
+                final product = _products[index];
+                return ListTile(
+                  leading: Thumbnail(imageUrl: product.image),
+                  title: Text(product.title),
+                  subtitle: Text(product.category),
+                  trailing: Text(product.price.toString()),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final categories = await _productService.getCategories();
+          _openBottomModal(context, categories);
+        },
+        child: Icon(Icons.tune),
+      ),
+    );
+  }
+
+  void _openBottomModal(BuildContext context, List<String> categories) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.8,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                trailing: TextButton(
+                    child: Text('Reset'),
+                    onPressed: () async {
+                      final updateProducts = await _productService.getAll();
+                      setState(() {
+                        _products = updateProducts;
+                      });
+                      Navigator.pop(context);
+                    }),
+                leading: Icon(Icons.filter_alt),
+                title: Text('Filter Categories'),
+              ),
+              Wrap(
+                spacing: 8.0,
+                children: categories
+                    .map(
+                      (category) => ElevatedButton(
+                        onPressed: () async {
+                          final updatedProducts =
+                              await _productService.getByCategory(category);
+                          setState(() {
+                            _products = updatedProducts;
+                          });
+                          Navigator.pop(context);
+                        },
+                        child: Text(category),
+                      ),
+                    )
+                    .toList(),
+              ),
+              ListTile(
+                leading: Icon(Icons.swap_vert),
+                title: Text('Sort By'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
